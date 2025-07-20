83ae6f018b3993ec6a6e3588028f89bb48e76e64
Liam-XD <liam.brotchie@xdesign.com>
2025-07-20 10:34:48 +0100

    Updated all test steps to use POM setup

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -3,17 +3,78 @@ import { type Page, type Locator, expect } from "@playwright/test";
 
 export class HomePage {
   // Use readonly to ensure the page object is immutable
+
+  // --- Locators ---
   readonly page: Page;
+  readonly mainHeading: Locator;
+  readonly heroBookNowButton: Locator;
+  readonly bookingAvailabilityCardHeading: Locator;
+  readonly checkInLabel: Locator;
+  readonly checkOutLabel: Locator;
+  readonly checkInTextbox: Locator;
+  readonly checkOutTextBox: Locator;
+  readonly checkAvailabilityButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.mainHeading = page.getByRole("heading", {
+      name: "Welcome to Shady Meadows B&B",
+    });
+    this.heroBookNowButton = page.getByRole("link", {
+      name: "Book Now",
+      exact: true, //Other 'book now' links have slightly different name
+    });
+    this.bookingAvailabilityCardHeading = page.getByRole("heading", {
+      name: "Check Availability & Book",
+    });
+    this.checkInLabel = page.getByText("Check In", { exact: true });
+    this.checkOutLabel = page.getByText("Check Out", { exact: true });
+    this.checkInTextbox = page
+      .locator("div")
+      .filter({ hasText: /^Check In$/ })
+      .getByRole("textbox");
+    this.checkOutTextBox = page
+      .locator("div")
+      .filter({ hasText: /^Check Out$/ })
+      .getByRole("textbox");
+    this.checkAvailabilityButton = page.getByRole("button", {
+      name: "Check Availability",
+    });
   }
 
+  // --- Actions & Assertions ---
   async gotoHome() {
     await this.page.goto("/");
   }
 
-  async assertStatus(status) {
+  async assertPageTitleAndHeading() {
+    await expect(this.page).toHaveTitle("Restful-booker-platform demo");
+    await expect(this.mainHeading).toBeVisible();
+  }
+
+  async assertHeroBookNowButtonIsReady() {
+    // Expect the primary booking button to be visible
+    await expect(this.heroBookNowButton).toBeVisible();
+    await expect(this.heroBookNowButton).toBeEnabled();
+  }
+
+  async assertBookingAvailabilityHeadingVisibility() {
+    await expect(this.bookingAvailabilityCardHeading).toBeVisible();
+  }
+
+  async verifyDateFieldsAreVisible() {
+    await expect(this.checkInLabel).toBeVisible();
+    await expect(this.checkOutLabel).toBeVisible();
+    await expect(this.checkInTextbox).toBeVisible();
+    await expect(this.checkOutTextBox).toBeVisible();
+  }
+
+  async checkAvailabilityButtonVisibleAndEnabled() {
+    await expect(this.checkAvailabilityButton).toBeVisible();
+    await expect(this.checkAvailabilityButton).toBeEnabled();
+  }
+
+  async assertStatus(status: unknown) {
     // Navigate to the URL and capture the response
     const response = await this.page.goto("/");
     // Assert that the response is not null and the status is 200 (OK)
--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -16,63 +16,29 @@ test.describe("Hero Section Verification", () => {
       await homePage.assertStatus(200);
     });
 
-    await test.step("Verify page title is correct", async () => {
-      await expect(page).toHaveTitle("Restful-booker-platform demo");
-    });
-
-    await test.step("Verify main heading is visible", async () => {
-      // Expect the main heading to be visible with the correct text
-      const heading = page.getByRole("heading", {
-        name: "Welcome to Shady Meadows B&B",
-      });
-      await expect(heading).toBeVisible();
+    await test.step("Verify page title & Heading is correct", async () => {
+      await homePage.assertPageTitleAndHeading();
     });
 
     await test.step('Verify "Book this room" button is present', async () => {
       // Expect the primary booking button to be visible
-      const bookButton = page.getByRole("link", {
-        name: "Book Now",
-        exact: true, //Other 'book now' links have slightly different name
-      });
-      await expect(bookButton).toBeVisible();
+      await homePage.assertHeroBookNowButtonIsReady();
     });
   });
 });
 
 test.describe("Booking Section Verification", () => {
-  test("Verify Booking card elements & functionality2", async ({ page }) => {
+  test("Verify Booking Availability card elements", async ({ page }) => {
     await test.step("Verify Card Heading", async () => {
-      await expect(
-        page.getByRole("heading", { name: "Check Availability & Book" })
-      ).toBeVisible();
-      await page.pause();
+      await homePage.assertBookingAvailabilityHeadingVisibility();
     });
 
     await test.step("Verify Check-in and Check-out fields", async () => {
-      await expect(page.getByText("Check In")).toBeVisible();
-      await expect(page.getByText("Check Out")).toBeVisible();
-      await expect(
-        page
-          .locator("div")
-          .filter({ hasText: /^Check In$/ })
-          .getByRole("textbox")
-      ).toBeVisible();
-      await expect(
-        page
-          .locator("div")
-          .filter({ hasText: /^Check Out$/ })
-          .getByRole("textbox")
-      ).toBeVisible();
+      await homePage.verifyDateFieldsAreVisible();
     });
 
     await test.step("Verify 'Check Availability' button is present and clickable", async () => {
-      await page.pause();
-      await expect(
-        page.getByRole("button", { name: "Check Availability" })
-      ).toBeVisible();
-      await expect(
-        page.getByRole("button", { name: "Check Availability" })
-      ).toBeEnabled();
+      await homePage.checkAvailabilityButtonVisibleAndEnabled();
     });
   });
 });
